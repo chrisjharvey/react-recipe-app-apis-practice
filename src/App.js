ce2c5757aec5fb5496dd1f60f0a62a308fa1492c
cjharvey <chris-harvey94@outlook.com>
2022-11-15 16:33:27 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import Pages from './pages/Pages';
+import Category from './components/Category';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Tasty</h1>
+      <Category />
+      <Pages />
     </div>
   );
 }
 
 export default App;
+
+// api_key = 9ecffcada173472c9f97ac07405fadb7
